3be5e4dbffe4f3307c9ba1bbd8bd362e732c287a
TokyoIncidents <tokyoincidents.g@gmail.com>
2012-01-17 02:32:28 +0900

    challenging to the yas/hippie-try-expand

--- dot.emacs.d/inits/40_yasnippet.el
+++ dot.emacs.d/inits/40_yasnippet.el
@@ -41,3 +41,17 @@
 (set-face-background  'yas/field-highlight-face nil)
 (set-face-underline-p 'yas/field-highlight-face t)
 (set-face-underline   'yas/field-highlight-face "#404040")
+
+;; hippie-expand
+(global-set-key "\C-o" 'hippie-expand)
+(setq hippie-expand-try-functions-list
+      '(yas/hippie-try-expand
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
