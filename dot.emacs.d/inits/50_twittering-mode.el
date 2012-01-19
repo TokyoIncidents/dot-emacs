f84d436f7a1f97e647d12da41a968e09b3b5c76e
TokyoIncidents <tokyoincidents.g@gamil.com>
2012-01-19 10:14:29 +0900

    add twittering-mode settings

--- dot.emacs.d/inits/50_twittering-mode.el
+++ dot.emacs.d/inits/50_twittering-mode.el
@@ -6,3 +6,16 @@
 (add-hook 'find-file-hooks 'alpaca-after-find-file)
 
 (setq twittering-use-master-password t)
+
+;; http://masutaka.net/chalow/2009-06-07-5.html
+;; (setq twittering-icon-mode t)
+(setq twittering-jojo-mode t)
+(defun twittering-mode-hook-func ()
+  (set-face-bold-p 'twittering-username-face t)
+  (set-face-foreground 'twittering-username-face "DeepSkyBlue3")
+  (set-face-foreground 'twittering-uri-face "gray35")
+  (define-key twittering-mode-map (kbd "<") 'my-beginning-of-buffer)
+  (define-key twittering-mode-map (kbd ">") 'my-end-of-buffer)
+  (define-key twittering-mode-map (kbd "F") 'twittering-favorite))
+
+(add-hook 'twittering-mode-hook 'twittering-mode-hook-func)
