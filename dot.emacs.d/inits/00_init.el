f1678deef81a25606a4363facaf558aa0637f0b2
TokyoIncidents <tokyoincidents.g@gmail.com>
2012-02-20 02:12:00 +0900

    add tab width change function

--- dot.emacs.d/inits/00_init.el
+++ dot.emacs.d/inits/00_init.el
@@ -40,7 +40,31 @@
 ;; indent settings
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
-(setq-default tab-width 4)
+;; (setq-default tab-width 4)
+
+;; http://masutaka.net/chalow/2009-07-10-4.html
+(defun set-aurora-tab-width (num &optional local redraw)
+  (interactive "nTab Width: ")
+  (when local
+    (make-local-variable 'tab-width)
+    (make-local-variable 'tab-stop-list))
+  (setq tab-width num)
+  (setq tab-stop-list ())
+  (while (<= num 256)
+    (setq tab-stop-list `(,@tab-stop-list ,num))
+    (setq num (+ num tab-width)))
+  (when redraw (redraw-display)) tab-width)
+
+(set-aurora-tab-width (setq default-tab-width (setq-default tab-width 4)))
+
+;; http://masutaka.net/chalow/2009-07-02-2.html
+(defvar ctl-q-map (make-keymap))                 ;;; (★a)
+(define-key global-map (kbd "C-q") ctl-q-map)    ;;; (★b)
+
+(define-key ctl-q-map (kbd "2") (lambda () (interactive) (set-aurora-tab-width 2 t t)))
+(define-key ctl-q-map (kbd "4") (lambda () (interactive) (set-aurora-tab-width 4 t t)))
+(define-key ctl-q-map (kbd "8") (lambda () (interactive) (set-aurora-tab-width 8 t t)))
+
 (c-set-offset 'case-label '+)
 
 ;; enable narrowing
